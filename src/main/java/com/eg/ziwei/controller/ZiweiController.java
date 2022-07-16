fa230a5a76b28788b9796daacafca13783c5bdf9
busydog1G <stu112114@gmail.com>
2022-07-16 21:23:30 +0800

    feat:初始化spring boot

---
+++ src/main/java/com/eg/ziwei/controller/ZiweiController.java
@@ -0,0 +1,13 @@
+package com.eg.ziwei.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ZiweiController {
+
+    @PostMapping
+    public void sampleInformation(){
+
+    }
+}
---
+++ src/test/java/com/eg/ziwei/ZiweiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eg.ziwei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiweiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
